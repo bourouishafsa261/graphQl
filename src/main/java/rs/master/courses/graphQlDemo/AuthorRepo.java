af4d5cebc4ee3a8da4d418e532ac2ed77d7799b5
bourouishafsa261 <bourouishafsa261@gmail.com>
2025-12-08 14:25:32 +0100

    add repositories

---
+++ src/main/java/rs/master/courses/graphQlDemo/AuthorRepo.java
@@ -0,0 +1,7 @@
+package rs.master.courses.graphQlDemo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepo extends JpaRepository<Author, Integer> {
+	
+}
---
+++ src/main/java/rs/master/courses/graphQlDemo/BookRepo.java
@@ -0,0 +1,7 @@
+package rs.master.courses.graphQlDemo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepo extends JpaRepository<Book, Integer> {
+	
+}
